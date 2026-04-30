38aa7e6f495a8f3d81c703f9215867f59741e2e3
Illia Redka <illaredka8@gmail.com>
2026-04-30 14:16:21 +0300

    Add Cook model

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator
 
 
 class DishType(models.Model):
@@ -9,3 +11,12 @@ class DishType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(50)]
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
