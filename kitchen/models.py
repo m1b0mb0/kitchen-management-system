ab1f115264aeae30ca6a1690315e95d66be3cf1e
Illia Redka <illaredka8@gmail.com>
2026-04-30 14:32:01 +0300

    Add Dish model

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(50)]
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
 
 
 class DishType(models.Model):
@@ -13,10 +23,22 @@ class DishType(models.Model):
         return self.name
 
 
-class Cook(AbstractUser):
-    years_of_experience = models.PositiveSmallIntegerField(
-        validators=[MaxValueValidator(50)]
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="dishes"
     )
 
+    class Meta:
+        ordering = ("name", )
+
     def __str__(self):
-        return f"{self.username} ({self.first_name} {self.last_name})"
+        return self.name
