57d41d017a83465d17874c5891b313a870cc52be
Illia Redka <illaredka8@gmail.com>
2026-05-03 19:52:47 +0300

    configure Django admin for dishes and ingredients
    - register Dish, Ingredient
    - add DishIngredient inline to Dish admin
    - add list_display to DishType admin panel

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from kitchen.models import Cook, DishType, Dish
+from kitchen.models import (
+    Cook,
+    DishType,
+    DishIngredient,
+    Dish,
+    Ingredient
+)
 
 
 @admin.register(Cook)
@@ -28,4 +34,24 @@ class CookAdmin(UserAdmin):
 
 @admin.register(DishType)
 class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+class DishIngredientInline(admin.TabularInline):
+    model = DishIngredient
+    extra = 1
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "dish_type")
+    list_filter = ("dish_type",)
+    search_fields = ("name",)
+    inlines = [DishIngredientInline]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name",)
     search_fields = ("name",)
