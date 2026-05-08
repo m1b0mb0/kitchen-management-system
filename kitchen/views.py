d24d4e97b810c3a1dc181370565e7c1c8cc9b9ac
Illia Redka <illaredka8@gmail.com>
2026-05-08 14:33:56 +0300

    Protect views with login_required and LoginRequiredMixin

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from kitchen.models import Cook, DishType, Dish, Ingredient
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     context = {
         "num_cooks": Cook.objects.count(),
@@ -15,25 +18,25 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "kitchen/index.html", context=context)
 
 
-class IngredientListView(generic.ListView):
+class IngredientListView(LoginRequiredMixin, generic.ListView):
     model = Ingredient
     paginate_by = 5
 
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     context_object_name = "dish_type_list"
     template_name = "kitchen/dish_type_list.html"
     paginate_by = 5
 
 
-class DishListView(generic.ListView):
+class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     queryset = Dish.objects.select_related("dish_type")
     paginate_by = 5
 
 
-class DishDetailView(generic.DetailView):
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
     model = Dish
     queryset = Dish.objects.select_related(
         "dish_type"
@@ -42,11 +45,11 @@ class DishDetailView(generic.DetailView):
     )
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
     paginate_by = 5
 
 
-class CookDetailView(generic.DetailView):
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
     model = Cook
     queryset = Cook.objects.prefetch_related("dishes__dish_type")
