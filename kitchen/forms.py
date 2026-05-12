20c337fe2540efd706db84b1fad9dba16bf65316
Illia Redka <illaredka8@gmail.com>
2026-05-12 15:21:23 +0300

    Add CookUpdateForm

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from kitchen.models import Cook
@@ -7,6 +8,16 @@ class CookCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Cook
         fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "years_of_experience"
+        )
+
+
+class CookUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = (
             "first_name",
             "last_name",
             "years_of_experience"
